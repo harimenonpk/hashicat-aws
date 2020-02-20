4292777b1a3074973d5947e180058a413129d57d
harimenonpk <31015275+harimenonpk@users.noreply.github.com>
2020-02-20 12:57:09 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,7 +45,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  ##default     = "placekitten.com"
-  default     = "akc.org"
+  default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
